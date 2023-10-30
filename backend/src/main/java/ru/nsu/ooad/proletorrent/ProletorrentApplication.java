4b3201b12e4f435a9fe5cb1e667ac789f0f04c92
Artyom-Kitov <artem.k3790@gmail.com>
2023-10-30 15:30:40 +0700

    feat: initialized backend

---
+++ backend/src/main/java/ru/nsu/ooad/proletorrent/ProletorrentApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.ooad.proletorrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProletorrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProletorrentApplication.class, args);
+	}
+
+}
